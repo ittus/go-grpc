a2fdc5c5f1c2afcd244915571c329e39ad351cd8
ittus <vuminhthang.cm@gmail.com>
2021-09-27 21:25:25 +0900

    feat(greet_service): boilerplate code

---
+++ generate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=source_relative greet/greetpb/greet.proto
