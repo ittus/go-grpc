70dd275057ec9a7823091085cda1b26b8cb481a6
ittus <vuminhthang.cm@gmail.com>
2021-09-28 10:35:23 +0900

    feat(calculator):  Reflection & CLI

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -12,6 +12,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
@@ -121,6 +122,9 @@ func main() {
 
 	s := grpc.NewServer()
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to server: %v", err)
